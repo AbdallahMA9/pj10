3efa728654a8a0ceef4604f8710a6eb9bcef5581
Abdallah <abdallah.merabet@outlook.com>
2024-08-26 20:30:36 +0200

    first_commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,17 +1,15 @@
 <?php
+
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`User`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -19,49 +17,34 @@ class User
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    #[Assert\NotBlank(message: "L'email ne doit pas être vide.")]
-    #[Assert\Email(
-        message: "L'adresse email '{{ value }}' n'est pas une adresse valide.",
-        checkMX: true
-    )]
     private ?string $email = null;
 
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message: "Le prénom ne doit pas être vide.")]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message: "Le nom ne doit pas être vide.")]
     private ?string $lastName = null;
 
     #[ORM\Column]
-    #[Assert\NotNull(message: "Le rôle ne doit pas être nul.")]
-    private ?bool $role = null;
-
-    #[ORM\Column]
-    #[Assert\NotNull(message: "La vérification ne doit pas être nulle.")]
     private ?bool $isVerified = null;
 
     #[ORM\Column]
-    #[Assert\NotNull(message: "La date de début ne doit pas être nulle.")]
-    #[Assert\Type("\DateTimeImmutable", message: "La date de début doit être une date valide.")]
     private ?\DateTimeImmutable $startedAt = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\NotBlank(message: "Le contrat ne doit pas être vide.")]
     private ?string $contract = null;
-
-    #[ORM\OneToMany(targetEntity: Crenaux::class, mappedBy: 'userId')]
-    private Collection $crenauxes;
-
-    #[ORM\OneToMany(targetEntity: Task::class, mappedBy: 'userId')]
-    private Collection $tasks;
-
-    public function __construct()
-    {
-        $this->crenauxes = new ArrayCollection();
-        $this->tasks = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -78,6 +61,64 @@ class User
         $this->email = $email;
 
         return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getFirstName(): ?string
@@ -100,18 +141,6 @@ class User
     public function setLastName(string $lastName): static
     {
         $this->lastName = $lastName;
-
-        return $this;
-    }
-
-    public function isRole(): ?bool
-    {
-        return $this->role;
-    }
-
-    public function setRole(bool $role): static
-    {
-        $this->role = $role;
 
         return $this;
     }
@@ -148,58 +177,6 @@ class User
     public function setContract(string $contract): static
     {
         $this->contract = $contract;
-
-        return $this;
-    }
-
-    public function getCrenauxes(): Collection
-    {
-        return $this->crenauxes;
-    }
-
-    public function addCrenaux(Crenaux $crenaux): static
-    {
-        if (!$this->crenauxes->contains($crenaux)) {
-            $this->crenauxes->add($crenaux);
-            $crenaux->setUserId($this);
-        }
-
-        return $this;
-    }
-
-    public function removeCrenaux(Crenaux $crenaux): static
-    {
-        if ($this->crenauxes->removeElement($crenaux)) {
-            if ($crenaux->getUserId() === $this) {
-                $crenaux->setUserId(null);
-            }
-        }
-
-        return $this;
-    }
-
-    public function getTasks(): Collection
-    {
-        return $this->tasks;
-    }
-
-    public function addTask(Task $task): static
-    {
-        if (!$this->tasks->contains($task)) {
-            $this->tasks->add($task);
-            $task->setUserId($this);
-        }
-
-        return $this;
-    }
-
-    public function removeTask(Task $task): static
-    {
-        if ($this->tasks->removeElement($task)) {
-            if ($task->getUserId() === $this) {
-                $task->setUserId(null);
-            }
-        }
 
         return $this;
     }
